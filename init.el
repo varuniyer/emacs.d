3bb0fddb256c68462195a9e251882519055746ff
Varun Iyer <humanvaruniyer@gmail.com>
2016-04-11 13:53:00 -0700

    init.el

---
+++ init.el
@@ -0,0 +1,333 @@
+(require 'package)
+(add-to-list 'package-archives
+			 '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(require 'eclim)
+(global-eclim-mode)
+
+(load-theme 'monokai t)
+(set-face-attribute 'default nil :font "Inconsolata-11")
+(set-frame-font "Inconsolata-11" nil t)
+
+(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(windmove-default-keybindings)
+(setq org-replace-disputed-keys t)
+
+(defconst lgfang-c-style
+  '((c-tab-always-indent        . t)
+    (c-basic-offset . 4)
+    (c-ignore-auto-fill . nil)
+    (c-comment-only-line-offset . (0 . 0))
+    (c-hanging-braces-alist     . ((substatement-open after before)
+                                   (brace-list-open)))
+    (c-hanging-colons-alist     . ((member-init-intro before)
+                                   (inher-intro)
+                                   (case-label after)
+                                   (label after)
+                                   (access-label after)))
+    (c-cleanup-list             . (scope-operator
+                                   empty-defun-braces
+                                   defun-close-semi))
+    (c-offsets-alist . ((knr-argdecl-intro . 5)
+                        (arglist-intro . +)
+                        (arglist-close . c-lineup-close-paren)
+                        (inclass . +)
+                        (member-init-intro . +)
+                        (statement-block-intro . +)
+                        (defun-block-intro . +)
+                        (substatement-open . 0)
+                        (label . 0)
+                        (statement-case-open . +)
+                        (statement-case-intro . +)
+                        (case-label . 0)
+                        (statement-cont . c-lineup-math)
+                        (inline-open . 0)
+                        (brace-list-open . +)
+                        (topmost-intro-cont . 0)
+                        (c . 1) ; "c" for continue of comment, not "c
+										; programming language"
+                        ))
+    (c-special-indent-hook . c-gnu-impose-minimum)
+    (c-block-comment-prefix . "lgf: ")
+    (c-comment-prefix-regexp . ((awk-mode . "#+(lgf: )?")
+                                (other ."lgf: \\|//+\\|\\**")))
+    ;; go to this file and test if c block comments works
+    ;; [[file:./patches/comments-test.c]]
+    (c-echo-syntactic-information-p . t))
+  "lgfang's C Programming Style")
+(c-add-style "lgfang" lgfang-c-style nil)
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (c-set-style "lgfang")
+            (c-toggle-hungry-state 1)
+            (hs-minor-mode 1)
+            (turn-on-cwarn-mode)
+            ;; (eldoc-mode 1)
+            ))
+
+(require 'nlinum)
+(global-nlinum-mode 1)
+(setq nlinum-format "%4d \u2502 ") ;; add spacing between numbering and text
+
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(setq gc-cons-threshold 100000000)
+(setq inhibit-startup-message t)
+
+(tool-bar-mode 0)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(defconst demo-packages
+  '(anzu
+    company
+    duplicate-thing
+    ggtags
+    helm
+										;helm-gtags
+    helm-projectile
+    helm-swoop
+    ;; function-args
+    clean-aindent-mode
+    comment-dwim-2
+    dtrt-indent
+    ws-butler
+    iedit
+    yasnippet
+    smartparens
+    projectile
+    volatile-highlights
+    undo-tree
+    zygospore))
+
+(defun install-packages ()
+  "Install all required packages."
+  (interactive)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (dolist (package demo-packages)
+    (unless (package-installed-p package)
+      (package-install package))))
+
+(install-packages)
+
+;; this variables must be set before load helm-gtags
+;; you can change to any prefix key of your choice
+										;(setq helm-gtags-prefix-key "\C-cg")
+
+(add-to-list 'load-path "~/.emacs.d/custom")
+
+;; (require 'setup-helm)
+;; (require 'setup-helm-gtags)
+;; (require 'setup-ggtags)
+;; (require 'setup-cedet)
+;; (require 'setup-editing)
+
+(windmove-default-keybindings)
+
+;; function-args
+;; (require 'function-args)
+;; (fa-config-default)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
+
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+;; company
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(delete 'company-semantic company-backends)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
+;; (define-key c-mode-map  [(control tab)] 'company-complete)
+;; (define-key c++-mode-map  [(control tab)] 'company-complete)
+
+;; company-c-headers
+;;(add-to-list 'company-backends 'company-c-headers)
+
+;; hs-minor-mode for folding source code
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
+
+;; Available C style:
+;; “gnu”: The default style for GNU projects
+;; “k&r”: What Kernighan and Ritchie, the authors of C used in their book
+;; “bsd”: What BSD developers use, aka “Allman style” after Eric Allman.
+;; “whitesmith”: Popularized by the examples that came with Whitesmiths C, an early commercial C compiler.
+;; “stroustrup”: What Stroustrup, the author of C++ used in his book
+;; “ellemtel”: Popular C++ coding standards as defined by “Programming in C++, Rules and Recommendations,” Erik Nyquist and Mats Henricson, Ellemtel
+;; “linux”: What the Linux developers use for kernel development
+;; “python”: What Python developers use for extension modules
+;; “java”: The default style for java-mode (see below)
+;; “user”: When you want to define your own style
+(setq
+ c-default-style "linux" ;; set style to "linux"
+ )
+
+
+
+(global-set-key (kbd "RET") 'newline-and-indent)  ; automatically indent when press RET
+
+;; activate whitespace-mode to view all whitespace characters
+(global-set-key (kbd "C-c w") 'whitespace-mode)
+
+;; show unncessary whitespace that can mess up your diff
+(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
+
+;; set appearance of a tab that is represented by 4 spaces
+(setq-default tab-width 4)
+(setq cua-auto-tabify-rectangles nil)
+(defadvice align (around smart-tabs activate)
+  (let ((indent-tabs-mode nil)) ad-do-it))
+(defadvice align-regexp (around smart-tabs activate)
+  (let ((indent-tabs-mode nil)) ad-do-it))
+(defadvice indent-relative (around smart-tabs activate)
+  (let ((indent-tabs-mode nil)) ad-do-it))
+(defadvice indent-according-to-mode (around smart-tabs activate)
+  (let ((indent-tabs-mode indent-tabs-mode))
+	(if (memq indent-line-function
+			  '(indent-relative
+				indent-relative-maybe))
+		(setq indent-tabs-mode nil))
+	ad-do-it))
+(defmacro smart-tabs-advice (function offset)
+  `(progn
+	 (defvaralias ',offset 'tab-width)
+	 (defadvice ,function (around smart-tabs activate)
+	   (cond
+		(indent-tabs-mode
+		 (save-excursion
+		   (beginning-of-line)
+		   (while (looking-at "\t*\\( +\\)\t+")
+			 (replace-match "" nil nil nil 1)))
+		 (setq tab-width tab-width)
+		 (let ((tab-width fill-column)
+			   (,offset fill-column)
+			   (wstart (window-start)))
+		   (unwind-protect
+			   (progn ad-do-it)
+			 (set-window-start (selected-window) wstart))))
+		(t
+		 ad-do-it)))))
+(smart-tabs-advice c-indent-line c-basic-offset)
+(smart-tabs-advice c-indent-region c-basic-offset)
+(smart-tabs-advice js2-indent-line js2-basic-offset)
+(smart-tabs-advice cperl-indent-line cperl-indent-level)
+(smart-tabs-advice python-indent-line-1 python-indent)
+(add-hook 'python-mode-hook
+		  (lambda ()
+			(setq indent-tabs-mode t)
+			(setq tab-width (default-value 'tab-width))))
+(smart-tabs-advice py-indent-line py-indent-offset)
+(smart-tabs-advice py-newline-and-indent py-indent-offset)
+(smart-tabs-advice py-indent-region py-indent-offset)
+(smart-tabs-advice ruby-indent-line ruby-indent-level)
+(setq ruby-indent-tabs-mode t)
+
+
+
+;; Compilation
+(global-set-key (kbd "<f5>") (lambda ()
+                               (interactive)
+                               (setq-local compilation-read-command nil)
+                               (call-interactively 'compile)))
+
+;; setup GDB
+(setq
+ ;; use gdb-many-windows by default
+ gdb-many-windows t
+
+ ;; Non-nil means display source file containing the main routine at startup
+ gdb-show-main t
+ )
+
+;; Package: clean-aindent-mode
+(require 'clean-aindent-mode)
+(add-hook 'prog-mode-hook 'clean-aindent-mode)
+
+;; Package: dtrt-indent
+(require 'dtrt-indent)
+(dtrt-indent-mode 1)
+
+;; Package: ws-butler
+(require 'ws-butler)
+(add-hook 'prog-mode-hook 'ws-butler-mode)
+
+;; Package: yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Package: smartparens
+(require 'smartparens-config)
+(setq sp-base-key-bindings 'paredit)
+(setq sp-autoskip-closing-pair 'always)
+(setq sp-hybrid-kill-entire-symbol nil)
+(sp-use-paredit-bindings)
+
+(show-smartparens-global-mode +1)
+(smartparens-global-mode 1)
+
+;; Package: projejctile
+(require 'projectile)
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+
+(require 'helm-projectile)
+(helm-projectile-on)
+(setq projectile-completion-system 'helm)
+(setq projectile-indexing-method 'alien)
+
+;; Set undo tree default
+(global-undo-tree-mode 1)
+
+;; Package zygospore
+(global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(emacs-eclim jdee ac-html-bootstrap ac-html edit-server jabber gnugo s angularjs-mode flycheck-google-cpplint go-mode swift-mode nlinum helm-gtags flycheck auto-complete monokai-theme zygospore undo-tree volatile-highlights smartparens yasnippet iedit ws-butler dtrt-indent comment-dwim-2 clean-aindent-mode helm-swoop helm-projectile helm ggtags duplicate-thing company anzu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'flymake)
+(add-hook 'java-mode-hook 'flymake-mode-on)
+(defun java-flymake-init ()
+  (list "javac" (list (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-with-folder-structure))))
+(add-to-list 'flymake-allowed-file-name-masks '("\\.java$" java-flymake-init flymake-simple-cleanup))
+(when (require 'flymake)
+  (set-variable 'flymake-log-level 9)
+  (setq flymake-start-syntax-check-on-newline nil)
+  (setq flymake-no-changes-timeout 10)
+  (add-hook 'java-mode-hook 'flymake-mode-on))
+
+(setq starttls-use-gnutls t
+      starttls-gnutls-program "/usr/local/bin/gnutls-cli"
+      starttls-extra-arguments nil)
+(setq jabber-account-list '(
+                            ("varun.iyer.353@chat.facebook.com"
+                             (:network-server . "chat.facebook.com")
+                             (:connection-type . starttls)
+                             (:port . 5222)
+                             )))
+
+;; Disable jabber.el presence notifications
+(remove-hook 'jabber-alert-presence-hooks
+             'sr-jabber-alert-presence-func)
