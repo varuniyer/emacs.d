19690c1e0aea731afc9d5c9c77867a260c5ded92
Varun Iyer <dev@varuniyer.us>
2021-11-18 23:28:10 -0800

    Update packages and Add AucTeX

---
+++ elpa/smooth-scroll-20130322.414/smooth-scroll-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from smooth-scroll.el  -*- no-byte-compile: t -*-
+(define-package "smooth-scroll" "20130322.414" "Minor mode for smooth scrolling and in-place scrolling." 'nil :commit "02320f28abb5cae28b3a18f6b9ce93129bdbfc45" :authors '(("K-talo Miyazaki <Keitaro dot Miyazaki at gmail dot com>")) :maintainer '("K-talo Miyazaki <Keitaro dot Miyazaki at gmail dot com>") :keywords '("convenience" "emulations" "frames") :url "http://www.emacswiki.org/emacs/download/smooth-scroll.el")
